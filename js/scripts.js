295ab52dd3c2e6721ce70a108d1ae5875697f001
silvermistshadow <bmead72@gmail.com>
2019-04-15 10:48:42 -0700

    refactor a little bit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 //Back-end logic
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
-      return true;
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
   } else {
-      return false;
+    return false;
   }
 
 }
